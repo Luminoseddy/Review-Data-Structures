cfcc6a92366cca9c28151380f40691d02044b258
Edward Palermo <eddx540@gmail.com>
2019-09-04 18:12:39 -0400

    Update IncrDecrArr

--- 00_Problems/IncrementDecrementArray/src/incrementdecrementarray/IncrementDecrementArray.java
+++ 00_Problems/IncrementDecrementArray/src/incrementdecrementarray/IncrementDecrementArray.java
@@ -1,5 +1,16 @@
 /**
  * 
+ * Given an array A of n distinct integer elements with the following property:
+ * The first k elements (0 < k < n-1) are in strictly increasing sequence followed by strictly decreasing sequence.
+ * 
+ * Example:
+ * A = {1, 3, 4, 5, 7, 14, 11, 7, 2, -4, -8}. It monotonically increases from 1 to 14, then decreases from 14 to -8.
+ * 
+ * (a) Implement an efficient program in Java that, given an array with the previous property,
+ *     determines whether a given integer is in the array.
+ * 
+ * (b) What is the running time complexity of the program? O(logn).
+ * 
  */
 package incrementdecrementarray;
 
@@ -38,7 +49,7 @@ public class IncrementDecrementArray {
        
         int mid = low + (high - low) / 2;            //  0 + (10 - 0) / 2   
         
-//        System.out.println("mid: "+mid+" low: "+low+" high: "+high);
+        System.out.println("mid: "+mid+" low: "+low+" high: "+high);
         
         if(high == low)                 /* Return highIndex if highestIndex == lowest Index */
             return high;     
@@ -47,7 +58,7 @@ public class IncrementDecrementArray {
             return max(a, mid+1, high); /* Recursively return (arrayList, the nextIndex value as the new low, the nextIndex value as the new high) */
               
         if(a[mid] > a[mid + 1])         /* IF currentIndexVal in list is GreaterThan than nextIndexVal. */
-            return max(a, low, mid);    /* Recursively return (arrayList, new low index, the new mid index as the highest index) */  
+            return max(a, low, mid);    /* Recursively, return (arrayList, new low index, the new mid index as the highest index) */  
         
         else 
             return mid;        
@@ -78,7 +89,8 @@ public class IncrementDecrementArray {
         
         else{
             middle = first + size/2;  
-//            System.out.println("middle: "+middle+" first: "+first+" size: "+size+" target: "+target);
+            
+            System.out.println("middle: "+middle+" first: "+first+" size: "+size+" target: "+target);
             
             if (target == a[middle])    /* IF the target we're looking for = the array of that index value, return middle. */
                 return middle;
@@ -87,22 +99,27 @@ public class IncrementDecrementArray {
                 return binarySearch_1(a, first, size/2, target);
             
             else
-                return binarySearch_1(a, middle+1, (size-1)/2, target); /*  */       
+                return binarySearch_1(a, middle+1, (size-1)/2, target); /* */       
         }                      
     }
-    
-    
     
      /**
      * Binary Search for the decreasing sequence.
      * 
      * Cycle 1: first: 0, size: k+1 = 5+1, target: -6
-     *          else -> middle = first + size/2    ==>    0 + 6/2     ==>   3 
-     *              IF  (target < a[middle])       ==>    (-6 < 5)    ==>   return binarySearch_1(a, 0, 3, -6); 
-     *                  
-     * Cycle 2: first: 0, size: , target: -6
-     *          else -> middle = first + size/2    ==>    0 + 3/2     ==>   1
-     *              IF (target < a[middle])        ==>    (-6 < 3)    ==>   return binarySearch_1(a, 0, 1, -6);
+     *          else ->
+     *              middle = 0 + 6/2 ==>  3
+     *              else ->      
+     *                  return binarySearch_2(a, 3+1, (6-1)/2, -6); 
+     * 
+     * Cycle 2: first: 4, size: 2, target: -6
+     *          else ->
+     *              middle = 4 + 2/2 ==>  5
+     *              else ->              
+     *                  return binarySearch_2(a, 5+1, (2-1)/2, -6);  
+     * 
+     * Cycle 3: first: 6, size: 0, target: -6
+     *          RETURN -1.
      * 
      */
     public static int binarySearch_2 (int[] a, int first, int size, int target){
@@ -114,7 +131,9 @@ public class IncrementDecrementArray {
         
         else{
             middle = first + size/2;
+            
             System.out.println("middle: "+middle+" first: "+first+" size: "+size+" target: "+target);
+            
             if (target == a[middle])
                 return middle;
             
