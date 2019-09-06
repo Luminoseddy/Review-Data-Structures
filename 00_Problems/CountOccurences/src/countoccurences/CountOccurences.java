0772548fb7241201d53ee88063485ff4694235ab
Edward Palermo <eddx540@gmail.com>
2019-09-06 13:26:32 -0400

    QuickSort
    Optimized.

--- 00_Problems/CountOccurences/src/countoccurences/CountOccurences.java
+++ 00_Problems/CountOccurences/src/countoccurences/CountOccurences.java
@@ -1,8 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Given a sorted array of integer, A, with possible duplicate elements.
+ * 
+ * (a) Implement an efficient function in Java to find in A, the numbers of occurrences of the input value k.
+ * 
+ *     For example, in an array A = {-1, 2, 3, 5, 6, 6, 6, 9, 10} and k = 6, your program should return 3.
+ * 
+ * (b) What is the running time complexity? --> O(log n).
+ * 
+ * Main Idea: 
+ * Find the left and right most occurrences of that value X in the array using a bianry search.
+ * 
  */
+    
 package countoccurences;
 
 /**
--- 03_Sorting&Searching/QuickSort/src/quicksort/QuickSort.java
+++ 03_Sorting&Searching/QuickSort/src/quicksort/QuickSort.java
@@ -1,86 +1,88 @@
 /**
  * A Divide and Conquer type of algorithm. 
  * We choose a random pivot in the list. 
+ * 
+ * Not adaptive, can't break automatically if sorted already without it completing its full cycle. 
+ * Not a stable sorting algorithm.
+ * 
+ * Main Idea:
+ *      1. Always pick the first pivot.
+ *      2. The key in Quicksort is the partition().
+ *      3. Partition: Given an array, and X, pivot of the array, place X in the correct position
+ *                    in the sorted array, then place all smaller elements in the left, and 
+ *                    greater elements on the right OF THAT PIvot point.
+ * 
+ * 
+ * Time complexity, O(NlogN)
  */
 package quicksort;
 
 public class QuickSort {
 
     private static int listToSort[] = new int[] {6, 5, 11, 4, 2, 5, 10, 3, 7, 8, 9};
-
+    private static int l = listToSort.length;
+    
     public static void main(String[] args) {
-        print(listToSort);
-        quickSort(listToSort, 0, listToSort.length - 1);
+        
+        quickSort(listToSort, 0, l - 1);
         print(listToSort);
     }
 
+    /* Utility type function. */
     public static void print(int[] listToSort) {
-        for (int el : listToSort) {
-            System.out.print(el + ",");
+        int x = listToSort.length;
+        for(int i = 0; i < x; ++i){
+            System.out.print(listToSort[i]+ " ");
         }
-        System.out.println();
     }
 
-    public static void swap(int[] listToSort, int iIndex, int jIndex) {
-        int temp = listToSort[iIndex];
-        listToSort[iIndex] = listToSort[jIndex];
-        listToSort[jIndex] = temp;
-    }
-
-    /**
-     * Processes recursion to sort the sub-Lists. 
-     * 
-     */
+    /* Processes recursion to sort the sub-Lists. */
     public static void quickSort(int[] listToSort, int low, int high) {
-        if (low >= high) {
-            return;
-        }
-        int pivotIndex = partition(listToSort, low, high);
-        quickSort(listToSort, low, pivotIndex - 1);
-        quickSort(listToSort, pivotIndex + 1, high);
+        /* Stop the sorting if the lower index is not lower than the high Index. */
+        if (low < high) {
+            
+            /* Finds the pivot in the array. */
+            int pivotIndex = partition(listToSort, low, high);
+
+            /* Recursively, sort all elements before pivot point. */
+            quickSort(listToSort, low, pivotIndex - 1);
+
+            /* Recursively, sort all elements after pivot point. */ 
+            quickSort(listToSort, pivotIndex + 1, high);
+        }   
     }
 
-    /**
-     * Finds the pivot, and moves elements to before or after the pivot.
-     * 
-     * Algorithm:
-     *      1. Create a function partition that takes in 3 parameters (int[] listToSort, int low, int high)
-     *         (Low & High specify indices which determine the portion of the list.)
-     *      2. 
-     */
-    
+    /* Finds the pivot, and moves elements to before or after the pivot. */
     public static int partition(int[] listToSort, int low, int high) {
-        int pivot = listToSort[low]; /* Choose the pivot to partition the list, low: loswet element in the index of the list. */
-        int l = low;
+        /* Element/pivot placed on the left position */
+        int pivot = listToSort[high]; 
+        int i = (low - 1); /* Index of the smaller element. */
         int h = high;
-        while (l < h) {
-            /* Moving from either end of the list towards the center we compare the elements to the pivot. */
-            /* Continues as long as the indices don't cross at the center.  */
-            while (listToSort[l] <= pivot && l < h) { 
-                System.out.println("Index: " + l +" w/ val: "+listToSort[l]);
-                printSI("Int l_Index: ", l);
-                printSI("Int h_Index: ", h);
-                printSI("Pivot: ", pivot);
-                l++;
+        
+        for (int j = low; j < high; j++){
+            
+            /* If the current element is smaller than the pivot. */
+            if(listToSort[j] < pivot){
+                i++;
+                int temp = listToSort[i];
+                listToSort[i] = listToSort[j];
+                listToSort[j] = temp;
             }
-            printS("");
-            while (listToSort[h] > pivot) {
-                System.out.println("listToSort[h] > " + listToSort[h] + " pivot " +pivot);
-                h--;
-            }
-            printS("");
-            if (l < h) {
-                swap(listToSort, l, h);
-            }
-            printS("");
         }
-        swap(listToSort, low, h);
+        
+        /* Swaping listToSort[i+1] and pivot. */
+        int temp = listToSort[i +1];
+        listToSort[i+1] = listToSort[h];
+        listToSort[h] = temp;
 
-        // System.out.println("Pivot2: " + pivot);
-        print(listToSort);
-        return h;
+        return i + 1;
     }
     
+//    public static void swap(int[] listToSort, int iIndex, int jIndex) {
+//        int temp = listToSort[iIndex];
+//        listToSort[iIndex] = listToSort[jIndex];
+//        listToSort[jIndex] = temp;
+//    }
     
     public static void printSI (String a, int b){
         System.out.println(a + b);
